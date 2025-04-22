4025e7bb7244debb65d5aedb9f12cd88b7923a6e
kohyeokjae <kohj0686@naver.com>
2025-04-22 14:17:53 +0900

    JDBC 1일차

---
+++ src/main/java/main/java/dao/UserDAO.java
@@ -0,0 +1,16 @@
+package main.java.dao;
+
+// === DAO 패턴 적용 ===
+// 1. DAO 패턴 정의
+// : Data Access Object
+// - DB와의 접근 로직을 하나의 클래스로 '캡슐화'하여 애플리케이션의 다른 부분과
+//   , DB 처리 코드를 분리하는 설계 패턴
+
+// 2. DAO 사용 목적
+// 1) DB 처리만 전담하는 DAO 클래스 생성
+// 2) DAO 클래스만 수정하면 DB 처리 수정이 가능
+//    >> 구조 명확, 유지보수 용이
+
+
+public class UserDAO {
+}
